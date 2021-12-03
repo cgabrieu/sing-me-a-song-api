1ee33448092b624fe9791fd64f73654669902383
Carlos Gabriel <carlosgabrielpc@live.com>
2021-12-03 19:03:39 -0300

    chore(lib):install get-youtube-id

--- src/controllers/recommendationController.js
+++ src/controllers/recommendationController.js
@@ -1,8 +1,11 @@
+
+
 async function postRecommendation(req, res) {
     try {
         const { name, youtubeLink } = req.body;
 
-        
+        const { error } = 
+
     } catch (error) {
         console.log(err);
         return res.sendStatus(500);
---
+++ src/schemas/recommendationSchemas.js
@@ -0,0 +1,6 @@
+import joi from "joi";
+
+const postRecommendationSchema = joi.object({
+    name: joi.string().required(),
+    youtubeLink: 
+});
