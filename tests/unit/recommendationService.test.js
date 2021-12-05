feab0fc22592f9a45792ce03bbf6bd330986b59c
Carlos Gabriel <carlosgabrielpc@live.com>
2021-12-05 19:28:20 -0300

    tests: create post recommendation tests

--- tests/unit/recommendationService.test.js
+++ tests/unit/recommendationService.test.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 import * as recommendationService from '../../src/services/recommendationService.js';
 import * as recommendationRepository from '../../src/repositories/recommendationRepository.js';
+import Conflict from '../../src/errors/Conflict.js';
 
 const sut = recommendationService;
 
@@ -15,16 +16,30 @@ const mockRecommendationRepository = {
   remove: (f) => jest.spyOn(recommendationRepository, 'remove').mockImplementationOnce(f),
 };
 
+const mockSong = {
+  name: 'Louis Armstrong - What a Wonderful World',
+  youtubeLink: 'https://www.youtube.com/watch?v=CWzrABouyeE',
+};
+
 describe('Unit Tests - Post Recommendation', () => {
   it('should return the added object', async () => {
-    const mockSong = {
-      name: 'Louis Armstrong - What a Wonderful World',
-      youtubeLink: 'https://www.youtube.com/watch?v=CWzrABouyeE',
-    };
-
     mockRecommendationRepository.findYoutubeLink(() => null);
-    mockRecommendationRepository.add(() => (mockSong));
+    mockRecommendationRepository.add(() => mockSong);
     const result = await sut.post();
     expect(result).toMatchObject(mockSong);
+  });
+
+  it('should return a conflict error when the youtube link already exists', async () => {
+    mockRecommendationRepository.findYoutubeLink(() => mockSong);
+    const promise = sut.post();
+    expect(promise).rejects.toThrow(Conflict);
+  });
+});
+
+describe('Unit Tests - Vote Recommendation', () => {
+  it('should return the added object with score +1 when up vote', async () => {
+    mockRecommendationRepository.findId(() => mockSong);
+    mockRecommendationRepository.vote(() => mockSong);
+    expect(result).toMatchObject();
   });
 });
