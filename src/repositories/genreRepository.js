36f6b69bdea13fd9a1f6088561da451417927f42
Carlos Gabriel <carlosgabrielpc@live.com>
2021-12-06 12:49:58 -0300

    feat: create genre add genre service and repository

--- src/repositories/genreRepository.js
+++ src/repositories/genreRepository.js
@@ -1,10 +1,17 @@
 import connection from '../database/database.js';
 
-// eslint-disable-next-line import/prefer-default-export
 export async function findName(name) {
   const result = await connection.query(
     'SELECT * FROM genres WHERE name = $1;',
     [name],
   );
   return result?.rows[0];
+}
+
+export async function add(name) {
+  const result = await connection.query(
+    'INSERT INTO genres (name) VALUES ($1) RETURNING *;',
+    [name],
+  );
+  return result.rows[0];
 }
