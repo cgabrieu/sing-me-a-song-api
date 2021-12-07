a5073596ba0f16d34384e8cde874279e5b1093f0
Matheus Galdino <matheus@respondeai.com.br>
2021-12-07 12:49:32 -0300

    preparando o review

--- src/schemas/genreSchema.js
+++
@@ -1,8 +0,0 @@
-import joi from 'joi';
-
-const postGenre = joi.object({
-  name: joi.string().min(1).max(50).required(),
-});
-
-// eslint-disable-next-line import/prefer-default-export
-export { postGenre };
